f0a7fc08dc5ae10eb6b0b9d38159521631176c3f
AndrewASO <124898597+AndrewASO@users.noreply.github.com>
2023-04-10 14:22:45 -0400

    Add files via upload

---
+++ enemy.h
@@ -0,0 +1,35 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+#include "../entity.h"
+#include "../../texturemanager.h"
+#include "../../level/level.h"
+
+class Enemy : public Entity {
+    private:
+        int textureIndex = 0;
+        sf::Sprite sprite;
+        //sf::Vector2f position;
+        TextureManager textureManager;
+        Player player;
+    public:
+        Enemy();
+        Stats stats;
+        sf::Vector2f position;
+        virtual void update(int tick, Level level) {};
+        void render(sf::RenderWindow* window) {};
+        void setPosition(sf::Vector2f position) {};
+        int distance() {};
+        sf::Sprite getSprite() {};
+        sf::Vector2f getPosition() {};
+
+        //Enemy movement
+        void move(){};
+
+        //Enemy atk
+        void attack(){};
+};
+#endif
